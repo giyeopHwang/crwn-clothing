b931fe4968cd36c5607e2ef82c7185b047c3161a
Giyeop Hwang <lay_ers@Giyeops-MacBook-Air.local>
2023-11-15 19:41:42 +0900

    feat(checkout): sets up a checkout page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -9,6 +9,8 @@ const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
   const { addItemToCart } = useContext(CartContext);
 
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={name} />
@@ -16,7 +18,7 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted" onClick={() => addItemToCart(product)}>
+      <Button buttonType="inverted" onClick={addProductToCart}>
         Add to cart
       </Button>
     </div>
